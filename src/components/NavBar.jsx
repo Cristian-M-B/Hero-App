4830d3018da44fcb24d385a6915f478614d19ea2
Cristian-M-B <crisbaronetto@hotmail.com>
2022-05-02 19:08:24 -0300

    add LogoutButton component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import SearchBar from './SearchBar.jsx';
+import LogoutButton from './LogoutButton.jsx';
+import { useStore } from '../context/StoreProvider.js';
 import { Container, Navbar, Nav } from 'react-bootstrap';
 
 export default function NavBar() {
+    const { token } = useStore();
+
     return (
         <Navbar bg="dark" variant="dark" className='w-100'>
             <Container>
                 <Navbar.Brand>Hero App</Navbar.Brand>
                 <Nav className="me-auto">
-                    <SearchBar />
+                    {token && <LogoutButton />}
                 </Nav>
             </Container>
         </Navbar>
