e861324274161e5e0007c468cb48cbdf88d2d6b5
Cristian-M-B <crisbaronetto@hotmail.com>
2022-05-05 14:57:52 -0300

    add options

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LogoutButton from './LogoutButton.jsx';
 import { useStore } from '../context/StoreProvider.js';
 import { Container, Navbar, Nav } from 'react-bootstrap';
@@ -10,9 +11,21 @@ export default function NavBar() {
         <Navbar bg="dark" variant="dark" className='w-100'>
             <Container>
                 <Navbar.Brand>Hero App</Navbar.Brand>
-                <Nav className="me-auto">
-                    {token && <LogoutButton />}
-                </Nav>
+                {/* <Nav className="me-auto"> */}
+                {token &&
+                    <Link to='/' className='text-decoration-none text-light'>
+                        Home
+                    </Link>
+                }
+                {token &&
+                    <Link to='/search' className='text-decoration-none text-light'>
+                        Search
+                    </Link>
+                }
+                {token &&
+                    <LogoutButton />
+                }
+                {/* </Nav> */}
             </Container>
         </Navbar>
     )
