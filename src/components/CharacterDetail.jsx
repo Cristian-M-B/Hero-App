a3a5038acfaab4c7dc8b1ccd92e45fa2b89f921c
Cristian-M-B <crisbaronetto@hotmail.com>
2022-05-05 15:00:19 -0300

    CharacterDetail component

---
+++ src/components/CharacterDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function CharacterDetail() {
+    return (
+        <div 
+            className='d-flex
+                flex-row
+                justify-content-center 
+                align-items-center
+                flex-wrap
+                min-vh-100'
+        >
+            CharacterDetail
+        </div>
+    )
+}
