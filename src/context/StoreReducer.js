8d1958224ac7f4a80b6896b5e21e1399725e20e8
Cristian-M-B <crisbaronetto@hotmail.com>
2022-05-05 15:01:37 -0300

    add actions

--- src/context/StoreReducer.js
+++ src/context/StoreReducer.js
@@ -9,36 +9,58 @@ export const actionsTypes = {
     GET_TOKEN: 'GET TOKEN',
     SET_TOKEN: 'SET TOKEN',
     LOGOUT: 'LOGOUT',
-    SEARCH: 'SEARCH'
+    SEARCH: 'SEARCH',
+    ADD_TEAM: 'ADD TEAM',
+    REMOVE_TEAM: 'REMOVE TEAM'
 }
 
 export default function StoreReducer(state, action) {
     switch(action.type){
 
         case actionsTypes.GET_TOKEN:
-            let response = JSON.parse(localStorage.getItem('token'));
+            let response = JSON.parse(localStorage.getItem('heroApp'));
             return {
-                ...state,
-                token: response? response: state.token
+                token: response? response.token: state.token,
+                team: response? response.team: state.team,
+                search: response? response.search: state.search,
+                detail: state.detail
             }
 
         case actionsTypes.SET_TOKEN:
-            localStorage.setItem('token', JSON.stringify(action.payload));
+            localStorage.setItem('heroApp', JSON.stringify({...state, token: action.payload}));
             return {
                 ...state,
                 token: action.payload
             }
 
         case actionsTypes.LOGOUT:
+            localStorage.setItem('heroApp', JSON.stringify({...state, token: null}));
             return {
                 ...state,
                 token: null
             }
 
         case actionsTypes.SEARCH:
+            localStorage.setItem('heroApp', JSON.stringify({...state, search: action.payload}));
             return {
                 ...state,
                 search: action.payload
+            }
+
+        case actionsTypes.ADD_TEAM:
+            let addTeam = [...state.team, action.payload];
+            localStorage.setItem('heroApp', JSON.stringify({...state, team: addTeam}));
+            return {
+                ...state,
+                team: addTeam
+            }
+
+        case actionsTypes.REMOVE_TEAM:
+            let deleteTeam = state.team.filter(character => character.id !== action.payload);
+            localStorage.setItem('heroApp', JSON.stringify({...state, team: deleteTeam}));
+            return {
+                ...state,
+                team: deleteTeam
             }
             
         default:
