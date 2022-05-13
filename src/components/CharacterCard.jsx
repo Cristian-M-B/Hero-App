544dcff8300463dbb169e584bbe9a46623fa9fa6
Cristian-M-B <crisbaronetto@hotmail.com>
2022-05-13 11:53:38 -0300

    add powerstats

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -9,29 +9,36 @@ import Alignment from '../utils/Alignment.js';
 
 export default function CharacterCard({ character, addButton, deleteButton, characterDetail }) {
     return (
-        <Card style={{ width: '300px' }} className='m-3' bg='dark' text='light'>
+        <Card style={{ width: '300px' }} className='m-3 p-2' bg='dark' text='light'>
             <Card.Img variant='top' src={character.image.url} style={{ height: '250px' }} />
             <Card.Body>
-                <Card.Title className='text-center'>{character.name}</Card.Title>
-                <Card.Text>{character.biography.alignment}</Card.Text>
-                {addButton && 
-                    <AddButton 
-                        disabled={IsRepeated(character.id) || Limit() || Alignment(character.biography.alignment)}
-                        character={character}
-                    />
-                }
-                {characterDetail &&
-                    <Link to={`/detail/${character.id}`}>
-                        <Button>
-                            Detail
-                        </Button>
-                    </Link>
-                }
-                {deleteButton && 
-                    <DeleteButton 
-                        id={character.id} 
-                    />
-                }
+                <Card.Title className='text-center' style={{ marginBottom: '20px' }}>{character.name}</Card.Title>
+                <Card.Text><strong>Intelligence: </strong>{character.powerstats.intelligence}</Card.Text>
+                <Card.Text><strong>Strength: </strong>{character.powerstats.strength}</Card.Text>
+                <Card.Text><strong>Speed: </strong>{character.powerstats.speed}</Card.Text>
+                <Card.Text><strong>Durability: </strong>{character.powerstats.durability}</Card.Text>
+                <Card.Text><strong>Power: </strong>{character.powerstats.power}</Card.Text>
+                <Card.Text><strong>Combat: </strong>{character.powerstats.combat}</Card.Text>
+                <div className='d-flex flex-row justify-content-end' style={{marginTop: '20px'}}>
+                    {addButton &&
+                        <AddButton
+                            disabled={IsRepeated(character.id) || Limit() || Alignment(character.biography.alignment)}
+                            character={character}
+                        />
+                    }
+                    {characterDetail &&
+                        <Link to={`/detail/${character.id}`}>
+                            <Button>
+                                Detail
+                            </Button>
+                        </Link>
+                    }
+                    {deleteButton &&
+                        <DeleteButton
+                            id={character.id}
+                        />
+                    }
+                </div>
             </Card.Body>
         </Card>
     )
